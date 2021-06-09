de333fb3f37c358141f0643d99ab51ac5e25d11c
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-09 19:37:10 +0530

    Added: Edit existing contact using name

--- src/main/java/service/AddressBook.java
+++ src/main/java/service/AddressBook.java
@@ -36,6 +36,47 @@ public class AddressBook {
         personList.add(person);
     }
 
+    /**
+     * Edit existing contact using their name
+     */
+    public static void edit() {
+        System.out.println("Enter the name of person to edit");
+        String firstName = scan.next();
+        ArrayList<Person> filteredList = new ArrayList<>();
+
+        for (int i = 0; i < personList.size(); i++) {
+            if (personList.get(i).getFirstname().equalsIgnoreCase(firstName)) {
+                filteredList.add(personList.get(i));
+            } else {
+                System.out.println("Record not exist");
+            }
+        }
+        if (null != filteredList && filteredList.size() >= 1) {
+            Person person = filteredList.get(0);
+            System.out.println("Enter last Name ");
+            person.setLastName(scan.nextLine());
+            System.out.println("Enter Address ");
+            person.setAddress(scan.nextLine());
+            System.out.println("Enter State ");
+            person.setState(scan.nextLine());
+            System.out.println("Enter City");
+            person.setCity(scan.nextLine());
+            System.out.println("Enter Zip");
+            person.setZip(scan.nextInt());
+            System.out.println("Enter Phone");
+            person.setPhone(scan.nextInt());
+            System.out.println("Enter Email1");
+            person.setEmail(scan.nextLine());
+
+            for (int j = 0; j < personList.size(); j++) {
+                if (personList.get(j).getFirstname().equalsIgnoreCase(person.getFirstname())) {
+                    personList.set(j, person);
+                }
+            }
+            System.out.println(personList);
+        }
+    }
+
     public static void main(String[] args) {
 
         boolean isExit = false;
@@ -50,6 +91,7 @@ public class AddressBook {
                     break;
                 case 2:
                     //edit
+                    edit();
                     break;
                 case 3:
                     //delete
